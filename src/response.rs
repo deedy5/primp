e3b1484571550a4b79a4267047ec6945ddbec3f8
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-07-23 16:34:05 +0300

    Rename `plaintext` to `text_markdown`

--- src/response.rs
+++ src/response.rs
@@ -66,7 +66,7 @@ impl Response {
     }
 
     #[getter]
-    fn plaintext(&mut self, py: Python) -> PyResult<String> {
+    fn text_markdown(&mut self, py: Python) -> PyResult<String> {
         let raw_bytes = self.content.bind(py).as_bytes();
         let text = from_read(raw_bytes, 1080);
         Ok(text)
