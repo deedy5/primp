d4773e190f0c55e118794062d90e346178ccf598
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-12-23 12:45:52 +0300

    add trait CookiesTraits

--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,7 @@
-use foldhash::fast::RandomState;
+use std::str::FromStr;
+
 use anyhow::{Error, Ok};
+use foldhash::fast::RandomState;
 use indexmap::IndexMap;
 
 use rquest::header::{HeaderMap, HeaderName, HeaderValue};
@@ -18,7 +20,7 @@ impl HeadersTraits for IndexMap<String, String, RandomState> {
         self.iter()
             .map(|(k, v)| {
                 (
-                    HeaderName::from_bytes(k.as_bytes())
+                    HeaderName::from_str(k)
                         .unwrap_or_else(|k| panic!("Invalid header name: {k:?}")),
                     HeaderValue::from_str(v)
                         .unwrap_or_else(|v| panic!("Invalid header value: {v:?}")),
@@ -51,11 +53,24 @@ impl HeadersTraits for HeaderMap {
     }
 
     fn insert_key_value(&mut self, key: String, value: String) -> Result<(), Error> {
-        let header_name = HeaderName::from_bytes(key.as_bytes())
+        let header_name = HeaderName::from_str(key.as_str())
             .unwrap_or_else(|k| panic!("Invalid header name: {k:?}"));
-        let header_value = HeaderValue::from_bytes(value.as_bytes())
+        let header_value = HeaderValue::from_str(value.as_str())
             .unwrap_or_else(|k| panic!("Invalid header value: {k:?}"));
         self.insert(header_name, header_value);
         Ok(())
+    }
+}
+
+pub trait CookiesTraits {
+    fn to_string(&self) -> String;
+}
+
+impl CookiesTraits for IndexMap<String, String, RandomState> {
+    fn to_string(&self) -> String {
+        self.iter()
+            .map(|(k, v)| format!("{}={}", k, v))
+            .collect::<Vec<String>>()
+            .join("; ")
     }
 }
