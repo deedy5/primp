21ad894ea6c02b2345a71c799e0c7dc55e9f30ee
Aniket Yadav <aniket201521@gmail.com>
2024-12-25 02:37:23 +0530

    add literals for impersonate values

--- primp/primp.pyi
+++ primp/primp.pyi
@@ -1,4 +1,13 @@
-from typing import Dict, Optional, Tuple, Union
+from typing import Dict, Optional, Tuple, Union, Literal
+
+IMPERSONATE = Literal[
+    "chrome_100", "chrome_101", "chrome_104", "chrome_105", "chrome_106", "chrome_107", "chrome_108", "chrome_109", "chrome_114",
+    "chrome_116", "chrome_117", "chrome_118", "chrome_119", "chrome_120", "chrome_123", "chrome_124", "chrome_126", "chrome_127",
+    "chrome_128", "chrome_129", "chrome_130", "chrome_131",  "safari_ios_16.5", "safari_ios_17.2", "safari_ios_17.4.1",
+    "safari_15.3", "safari_15.5", "safari_15.6.1", "safari_16", "safari_16.5", "safari_17.0", "safari_17.2.1", "safari_17.4.1",
+    "safari_17.5", "safari_18", "safari_18.1.1", "safari_18.2", "safari_ipad_18", "okhttp_3.9", "okhttp_3.11", "okhttp_3.13",
+    "okhttp_3.14", "okhttp_4.9", "okhttp_4.10", "okhttp_5", "edge_101", "edge_122", "edge_127", "edge_131"
+]
 
 class Response:
     @property
@@ -36,7 +45,7 @@ class Client:
         cookie_store: Optional[bool] = True,
         referer: Optional[bool] = True,
         proxy: Optional[str] = None,
-        impersonate: Optional[str] = None,
+        impersonate: Optional[IMPERSONATE] = None,
         follow_redirects: bool = True,
         max_redirects: int = 20,
         verify: Optional[bool] = True,
@@ -167,7 +176,7 @@ def request(
     auth: Optional[Tuple[str, Optional[str],]] = None,
     auth_bearer: Optional[str] = None,
     timeout: Optional[float] = None,
-    impersonate: Optional[str] = None,
+    impersonate: Optional[IMPERSONATE] = None,
     verify: Optional[bool] = None,
     ca_cert_file: Optional[str] = None,
 ) -> Response: ...
@@ -179,7 +188,7 @@ def get(
     auth: Optional[Tuple[str, Optional[str]]] = None,
     auth_bearer: Optional[str] = None,
     timeout: Optional[float] = None,
-    impersonate: Optional[str] = None,
+    impersonate: Optional[IMPERSONATE] = None,
     verify: Optional[bool] = None,
     ca_cert_file: Optional[str] = None,
 ) -> Response: ...
@@ -191,7 +200,7 @@ def head(
     auth: Optional[Tuple[str, Optional[str]]] = None,
     auth_bearer: Optional[str] = None,
     timeout: Optional[float] = None,
-    impersonate: Optional[str] = None,
+    impersonate: Optional[IMPERSONATE] = None,
     verify: Optional[bool] = None,
     ca_cert_file: Optional[str] = None,
 ) -> Response: ...
@@ -203,7 +212,7 @@ def options(
     auth: Optional[Tuple[str, Optional[str]]] = None,
     auth_bearer: Optional[str] = None,
     timeout: Optional[float] = None,
-    impersonate: Optional[str] = None,
+    impersonate: Optional[IMPERSONATE] = None,
     verify: Optional[bool] = None,
     ca_cert_file: Optional[str] = None,
 ) -> Response: ...
@@ -215,7 +224,7 @@ def delete(
     auth: Optional[Tuple[str, Optional[str]]] = None,
     auth_bearer: Optional[str] = None,
     timeout: Optional[float] = None,
-    impersonate: Optional[str] = None,
+    impersonate: Optional[IMPERSONATE] = None,
     verify: Optional[bool] = None,
     ca_cert_file: Optional[str] = None,
 ) -> Response: ...
@@ -231,7 +240,7 @@ def post(
     auth: Optional[Tuple[str, Optional[str]]] = None,
     auth_bearer: Optional[str] = None,
     timeout: Optional[float] = None,
-    impersonate: Optional[str] = None,
+    impersonate: Optional[IMPERSONATE] = None,
     verify: Optional[bool] = None,
     ca_cert_file: Optional[str] = None,
 ) -> Response: ...
@@ -247,7 +256,7 @@ def put(
     auth: Optional[Tuple[str, Optional[str]]] = None,
     auth_bearer: Optional[str] = None,
     timeout: Optional[float] = None,
-    impersonate: Optional[str] = None,
+    impersonate: Optional[IMPERSONATE] = None,
     verify: Optional[bool] = None,
     ca_cert_file: Optional[str] = None,
 ) -> Response: ...
@@ -263,7 +272,7 @@ def patch(
     auth: Optional[Tuple[str, Optional[str]]] = None,
     auth_bearer: Optional[str] = None,
     timeout: Optional[float] = None,
-    impersonate: Optional[str] = None,
+    impersonate: Optional[IMPERSONATE] = None,
     verify: Optional[bool] = None,
     ca_cert_file: Optional[str] = None,
 ) -> Response: ...
